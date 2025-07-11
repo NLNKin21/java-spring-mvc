fc57d76824febff92c60eaf7efe37dd3d69da647
Nguyen Long Nhat <nlnhttl21@gmail.com>
2025-07-12 00:12:59 +0700

    Initial commit

---
+++ src/main/java/com/example/java_spring_mvc_hoidanit/JavaSpringMvcHoidanitApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_mvc_hoidanit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringMvcHoidanitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringMvcHoidanitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_spring_mvc_hoidanit/JavaSpringMvcHoidanitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_mvc_hoidanit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringMvcHoidanitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
